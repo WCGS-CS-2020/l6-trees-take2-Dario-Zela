37e7bc2b8814c798e1e32d1868701e1bc205d72c
Dario-Zela <58654337+Dario-Zela@users.noreply.github.com>
2020-03-03 12:56:34 +0000

    Initial Idea
    To fix delete node

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
+using System;
 
-namespace L6Trees
+namespace Binary_Trees
 {
 
     /*
@@ -14,16 +14,177 @@ namespace L6Trees
     class Node
     {
         // Attributes
-        private  Node left;
-        private  Node right;
+        private Node left = null;
+        private Node right = null;
         private string item;
 
         //Methods
-        public Node(string item) { }
-        public void addNode(string item) {}
-        public Boolean findNode(string item) { return true; }
-        public Boolean deleteNote(string item) { return true; }
-        void printTree() { }
+        private int toInt(string s)
+        {
+            int val = 0;
+            foreach (char c in s)
+            {
+                val += c;
+            }
+            return val;
+        }
+        public Node(string item) 
+        {
+            this.item = item;
+        }
+        public void addNode(string item)
+        {
+            Node cur = this;
+            while(true)
+            {
+                if (toInt(cur.item) <= toInt(item))
+                {
+                    if (cur.left == null)
+                    {
+                        cur.left = new Node(item);
+                        return;
+                    }
+                    else
+                    {
+                        cur = cur.left;
+                    }
+                }
+                else
+                {
+                    if (cur.right == null)
+                    {
+                        cur.right = new Node(item);
+                        return;
+                    }
+                    else
+                    {
+                        cur = cur.right;
+                    }
+                }
+            }
+        }
+        public Boolean findNode(string item) 
+        {
+            Node cur = this;
+            while (true)
+            {
+                if (toInt(cur.item) <= toInt(item))
+                {
+                    if (cur.left == null)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        if (cur.left.item == item)
+                        {
+                            return true;
+                        }
+                        cur = cur.left;
+                    }
+                }
+                else
+                {
+                    if (cur.right == null)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        if (cur.right.item == item)
+                        {
+                            return true;
+                        }
+                        cur = cur.right;
+                    }
+                }
+            } 
+        }
+        public Boolean deleteNote(string item) 
+        {
+            Node cur = this;
+            while (true)
+            {
+                if (toInt(cur.item) <= toInt(item))
+                {
+                    if (cur.left == null)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        if (cur.left.item == item)
+                        {
+                            if (cur.left.left == null && cur.left.right == null)
+                            {
+                                cur.left = null;
+                            }
+                            else
+                            {
+                                Node temp = new Node(cur.left.left.item);
+                                cur.left = temp;
+                                cur = cur.left.left;
+                                Node store = cur;
+                                while (cur.left != null)
+                                {
+                                    temp.addNode(cur.left.item);
+                                    cur = cur.left;
+                                }
+                                cur = store;
+                                while (cur.right != null)
+                                {
+                                    temp.addNode(cur.right.item);
+                                    cur = cur.right;
+                                }
+                            }
+                            return true;
+                        }
+                        cur = cur.left;
+                    }
+                }
+                else
+                {
+                    if (cur.right == null)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        if (cur.right.item == item)
+                        {
+                            if (cur.right.left == null && cur.right.right == null)
+                            {
+                                cur.left = null;
+                            }
+                            else
+                            {
+                                Node temp = new Node(cur.right.right.item);
+                                cur.right = temp;
+                                cur = cur.right.right;
+                                Node store = cur;
+                                while (cur.right != null)
+                                {
+                                    temp.addNode(cur.right.item);
+                                    cur = cur.right;
+                                }
+                                cur = store;
+                                while (cur.left != null)
+                                {
+                                    temp.addNode(cur.left.item);
+                                    cur = cur.left;
+                                }
+                            }
+                            return true;
+                        }
+                        cur = cur.right;
+                    }
+                }
+            } 
+        }
+        public void printTree()
+        {
+
+        }
     }
 
     class Program
@@ -47,7 +208,7 @@ namespace L6Trees
 
             // print out the tree using different traversal methods
             //
-
+            root.printTree();
             // Test the findNote() and deleteNode()
         }
     }
